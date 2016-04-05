c1e938c4d99b8f3893a969a20ad6bb602e943f8f
Ciço <mauriciocc@outlook.com>
2016-04-04 21:28:46 -0300

    Ajuste configuração banco

--- src/models/Orm.js
+++ src/models/Orm.js
@@ -1,13 +1,15 @@
 const Sequelize = require('sequelize');
 const dbConfig = rootRequire("config/config").production;
 
-const orm = new Sequelize(dbConfig.database, dbConfig.user, dbConfig.password, {
-    host: dbConfig.host,
-    port: dbConfig.port,
-    dialect: dbConfig.dialect,
+console.log(dbConfig);
+
+const orm = new Sequelize(dbConfig.database || 'pedidos', dbConfig.username || 'postgres', dbConfig.password || 'postgres', {
+    host: dbConfig.host || 'localhost',
+    port: dbConfig.port || 5432,
+    dialect: dbConfig.dialect || 'postgres',
     pool: {
-        max: dbConfig.poolSize,
-        min: dbConfig.poolSize,
+        max: dbConfig.poolSize || 4,
+        min: dbConfig.poolSize || 4,
         maxIdleTime: 24*60*60*1000
     }
 });
