14cc3a102b1afa54483a897c2dfdfb23b600e480
Ciço <mauriciocc@outlook.com>
2016-04-10 10:56:14 -0300

    Adicionadas funcionalidades sobreescritas na navbar ao header

---
+++ client/app/navbar/header.component.js
@@ -0,0 +1,8 @@
+import template from './header.html';
+import controller from './navbar.controller';
+import './header.scss';
+
+export default {
+  template,
+  controller
+};
--- client/app/navbar/navbar.js
+++ client/app/navbar/navbar.js
@@ -1,11 +1,12 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import navbarComponent from './navbar.component';
+import headerComponent from './header.component';
 
 let navbarModule = angular.module('navbar', [
   uiRouter
 ])
-
-.component('navbar', navbarComponent);
+.component('navbar', navbarComponent)
+.component('headerComponent', headerComponent);
 
 export default navbarModule;
