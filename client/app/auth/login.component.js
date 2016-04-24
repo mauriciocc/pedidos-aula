e9e2a72f92e36994650c5c8970353de71c4a2a8a
Ciço <mauriciocc@outlook.com>
2016-04-24 05:37:41 -0300

    Ajustando autenticação

--- client/app/auth/login.component.js
+++ client/app/auth/login.component.js
@@ -5,9 +5,11 @@ import "./login.scss";
 /*@ngInject*/
 class controller {
 
-  constructor(AuthService, $state) {
+  constructor(AuthService, Toast, $state) {
+    console.log(Toast);
     this.AuthService = AuthService;
     this.$state = $state;
+    this.Toast = Toast;
     this.credentials = {
       email: 'admin@gmail.com',
       password: 'admin',
@@ -17,7 +19,10 @@ class controller {
 
   doLogin() {
     this.AuthService.authenticate(this.credentials)
-      .then(() => this.$state.transitionTo('home'));
+      .then(
+        () => this.$state.transitionTo('home'),
+        () => this.Toast.warning('Usuário e/ou senha não encontrados')
+      );
   }
 
 }
--- client/app/user/crud/form.controller.js
+++ client/app/user/crud/form.controller.js
@@ -1,11 +1,12 @@
 /*@ngInject*/
 class UserFormController {
 
-  constructor(UserService, Toast, $mdDialog, $stateParams) {
+  constructor(UserService, Toast, $mdDialog, $stateParams, $state) {
     this.UserService = UserService;
     this.Toast = Toast;
     this.$mdDialog = $mdDialog;
     this.$stateParams = $stateParams;
+    this.$state = $state;
     if ($stateParams.id) {
       UserService.findOne($stateParams.id).then((user) => this.user = user);
     } else {
@@ -20,23 +21,11 @@ class UserFormController {
   save() {
     this.UserService.save(this.user).then(() => {
       this.Toast.success('Usuário salvo com sucesso!');
+        this.$state.go('users');
       this.refresh();
     }, () => {
       this.Toast.danger('Ocorreu um erro ao tentar salvar o usuário!' + arguments);
     });
-  }
-
-  remove(id) {
-    this.UserService.remove(id).then(() => this.refresh());
-  }
-
-  edit(id) {
-    this.$mdDialog.show(
-      this.$mdDialog.alert()
-        .title('This is an alert title')
-        .textContent('You can specify some description text in here.')
-        .ok('Salvar')
-    );
   }
 
 }
