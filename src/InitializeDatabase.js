06bf99e1d9be9892a2021d87415fc9390d5438bb
MiguelSWolf <soulmiguel@gmail.com>
2016-04-24 11:25:30 -0300

    script que corre a auditoria

--- src/InitializeDatabase.js
+++ src/InitializeDatabase.js
@@ -11,6 +11,28 @@ const Auditing = require('./models/Auditing');
 
 module.exports = function () {
   Orm.instance.sync({force: true}).then(() => {
+    var functionAuditing = "CREATE OR REPLACE FUNCTION auditing() "+
+	"RETURNS TRIGGER "+
+	"AS $$ BEGIN "+
+        "       INSERT INTO auditings (\"table\", \"id_refer\", \"values\", \"date\", \"createdAt\", \"updatedAt\") VALUES(TG_RELNAME, OLD.id, to_json(OLD), now(), now(), now());"+
+        "       IF TG_OP = 'UPDATE' THEN"+
+        "           RETURN NEW;"+
+        "       ELSE"+
+        "           RETURN OLD;"+
+	"   	END IF;"+
+	"END;$$ LANGUAGE plpgsql;";
+    Orm.instance.query(functionAuditing);
+    var triggersAuditing = ""+
+    "CREATE TRIGGER triggerAuditingAddresses BEFORE DELETE OR UPDATE ON addresses FOR EACH ROW EXECUTE PROCEDURE auditing();"+
+    "CREATE TRIGGER triggerAuditingCategories BEFORE DELETE OR UPDATE ON categories FOR EACH ROW EXECUTE PROCEDURE auditing();"+
+    "CREATE TRIGGER triggerAuditingCities BEFORE DELETE OR UPDATE ON cities FOR EACH ROW EXECUTE PROCEDURE auditing();"+
+    "CREATE TRIGGER triggerAuditingCustomers BEFORE DELETE OR UPDATE ON customers FOR EACH ROW EXECUTE PROCEDURE auditing();"+
+    "CREATE TRIGGER triggerAuditingProducts BEFORE DELETE OR UPDATE ON products FOR EACH ROW EXECUTE PROCEDURE auditing();"+
+    "CREATE TRIGGER triggerAuditingRoles BEFORE DELETE OR UPDATE ON roles FOR EACH ROW EXECUTE PROCEDURE auditing();"+
+    "CREATE TRIGGER triggerAuditingStock_entries BEFORE DELETE OR UPDATE ON stock_entries FOR EACH ROW EXECUTE PROCEDURE auditing();"+
+    "CREATE TRIGGER triggerAuditingUsers BEFORE DELETE OR UPDATE ON users FOR EACH ROW EXECUTE PROCEDURE auditing();";
+    Orm.instance.query(triggersAuditing);
+    
     Role.findOrCreate({defaults: {name: "Admin"}, where: {name: "Admin"}}).spread((instance, created) => {
       Role.Admin = instance;
 
