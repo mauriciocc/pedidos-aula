8ea7231d0c9898b00bacc408a6c7d5f2fffb3c88
Ciço <mauriciocc@outlook.com>
2016-06-06 21:44:54 -0300

    Ajuste build, ajuste header, ajuste na fila de processamento de estoque

--- src/service/StockService.js
+++ src/service/StockService.js
@@ -1,12 +1,33 @@
 var Promise = require("bluebird");
-var StockEntry = require('../models/StockEntry');
 
-var StockService = function () {
+var OperationQueue = function () {
 
   var queue = [];
+  var runningOperation;
 
-  this.runStockOperation = function (stockOperationFn) {
-       
+  var createPromise = function (fn) {
+    new Promise(resolve => {
+      var result = {};
+      try {
+        result = fn();
+      } catch (e) {
+      }
+      resolve(result);
+    }).finally(() => {
+      runningOperation = null;
+      if (queue.length > 0) {
+        runningOperation = createPromise(queue.shift());
+      }
+    });
   };
 
+  this.runOperation = (operationFn) => {
+    if (runningOperation) {
+      queue.push(operationFn);
+    }
+    runningOperation = createPromise(operationFn)
+  }
+
 };
+
+module.exports = OperationQueue;
